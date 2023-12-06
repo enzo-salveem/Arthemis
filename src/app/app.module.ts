5d6be8514143a458afb493f8f4f8792bbf17990b
Enzo Monsalve <enzomonsalveem99@gmail.com>
2023-12-06 01:24:18 -0300

    primer cambio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './componentes/barra-navegacion/barra-navegacion.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
