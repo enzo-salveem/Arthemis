9d1352fc034eae526f44d660b58c6dd099cc780c
Enzo Monsalve <enzomonsalveem99@gmail.com>
2023-12-17 23:09:04 -0300

    gradiante

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { ReglasComponent } from './pages/reglas/reglas.component';
 import { BeneficiosComponent } from './pages/beneficios/beneficios.component';
 import { GuerraComponent } from './pages/guerra/guerra.component';
 import { StrikesComponent } from './pages/strikes/strikes.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'strikes', component: StrikesComponent },
   { path: 'beneficios', component: BeneficiosComponent },
   { path: 'guerra', component: GuerraComponent },
+  { path: 'contacto', component: ContactoComponent },
 ];
 
 @NgModule({
